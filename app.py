5b411fed3ac767422a5053a571e53e2f317ff87c
abineshDh <abineshspartan2001@gmail.com>
2025-04-17 18:48:48 +0530

    added comments

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
+# global list to store tasks
 todo_tasks = []
 
+# Add task form 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
 def home():
@@ -15,6 +17,7 @@ def home():
             print("No Task to Add")
     return render_template('index.html', todo_tasks=todo_tasks)
 
+# Remove task form
 @app.route("/remove", methods=["POST"])
 def remove_one():
     task = request.form.get('single_task')
